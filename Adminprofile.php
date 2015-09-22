9addaab4d14aab44e5fa41a2aca5d6ac28edc830
lambdainnovphp <mydihemasundar@lambdainnov.com>
2015-09-22 12:24:05 +0530

    adminprofile

--- Adminprofile.php
+++ Adminprofile.php
@@ -5,6 +5,7 @@ $AdminID = $_SESSION['AdminId'];
 if($_SESSION['AdminId']==''){	
 	 header("Location: index"); 
 }
+
 ?>
 <style type="text/css">
 	.BoldText{
@@ -16,86 +17,82 @@ if($_SESSION['AdminId']==''){
 				<!-- Content Header (Page header) -->
 				<section class="content-header">
 					<h1>
-						Profile
+						<?php echo $lang['profile'];?>
 						<small></small>
 					</h1>
 					<ol class="breadcrumb">
-						<li><a href="#"><i class="fa fa-dashboard text-red"></i> Home</a></li>
-						<li><a href="#"><i class="fa fa-user text-red"></i> Profile</a></li>
+						<li><a href="#"><i class="fa fa-dashboard text-red"></i><?php echo $lang['Home']?> </a></li>
+						<li><a href="#"><i class="fa fa-user text-red"></i> <?php echo $lang['profile'];?></a></li>
 					</ol>
 				</section>
 					<?php 
 						$querys =	"SELECT * FROM AdminInfo WHERE AdminId='Adm001'";
 						$result_users = sqlsrv_query( $conn, $querys ,array(), array( "Scrollable" => SQLSRV_CURSOR_KEYSET ));
-											
-						while($row = sqlsrv_fetch_array($result_users)){?>
+
+						$row = sqlsrv_fetch_array($result_users)
+						
+						
+						?>
 	  
    
 				<!-- Main content -->
+				
 				<section class="content">
-					<div class="panel">
-						<div class="panel-body">
-							<div class="row">
-								<div class="col-md-3 col-sm-3">
-									<div class="user-wrapper col-md-8 col-md-offset-2">
-										<img src="images/profile" class="img-responsive img-circle"> 
-										<div class="description user-description text-center">
-										   <h4><?php  echo $row['AFullName_en'];?> </h4>
-										</div>
-									 </div>
-								</div>
-								<div class="col-md-9">
-									<div class="panel panel-info">
-										<div class="panel-heading">
-											<h3 class="panel-title"><span class="fa fa-user-secret"></span> <span class="fa fa-angle-right"></span> <?php  echo $row['AFullName_en'];?>  Profile</h3>
-										</div>
-										<div id="no-more-tables panel-body col-md-12">
-											<table class="table-bordered table-striped table-condensed cf">
-												<thead class="cf">
-													<tr>
-														<th></th>
-														<th></th>
-													</tr>
-												</thead>
-												<tbody>
-													<tr>
-														<td class="BoldText">Student ID</td>
-														<td><?php  echo $row['AdminId'];?></td>
-													</tr>
-													<tr>
-														<td class="BoldText">User Name</td>
-														<td><?php echo $row['AdminUserName'];?></td>
-													</tr>
-													<tr>
-														<td class="BoldText">First Name <div class="pull-right"><a href="#Eng">Eng</a> / <a href="#Ara">Ara</a></div></td>
-														<td><?php  echo $row['AFirstName_en'];?> / <?php  echo $row['AFirstName_ar'];?></td>
-													</tr>													
-													<tr>
-														<td class="BoldText">Last Name <div class="pull-right"><a href="#Eng">Eng</a> / <a href="#Ara">Ara</a></div></td>
-														<td><?php  echo $row['ALastName_en'];?> / <?php  echo $row['ALastName_ar'];?></td>
-													</tr>
-													<tr>
-														<td class="BoldText">DOB</td>
-														<td><?php  echo $row['DoB'];?></td>
-													</tr>
-													<tr>
-														<td class="BoldText">Full Name <div class="pull-right"><a href="#Eng">Eng</a> / <a href="#Ara">Ara</a></div></td>
-														<td><?php  echo $row['AFullName_en'];?>  <?php  echo $row['AFullName_ar'];?></td>
-													</tr>
-													<tr>
-														<td class="BoldText">Mobile</td>
-														<td><?php  echo $row['Mobile'];?></td>
-													</tr>
-												</tbody>
-											</table>
-										</div>
+					<div class="row">
+						<div class="col-md-12">
+							<div class="panel profile-bg">
+								<div class="panel-heading">
+									<h3 class="panel-title"> 	</h3>
+									<div class="pull-right">
+										<!--<a href="#Fakelink">Arabic </a> / <a href="#Fakelink">English</a>-->
 									</div>
 								</div>
-							</div>
+								<hr>
+								<div class="panel-body">
+									<div class="profile-body">
+										<img src="assets/images/sample-student.png" class="img-circle">
+										<div class="clearfix"></div>
+										<h2 class="title" data-toggle="tooltip" data-placement="top" title="Student Name"> <?php  echo $row['AFullName_en'];?></h2>
+										<h4 class="student-id" data-toggle="tooltip" data-placement="top" title="Student ID"></h4>
+										<hr>
+										<table class="table-responsive">
+											<tr>
+												<td>Admin ID</td>
+												<td>: <?php  echo $row['AdminId'];?></td>
+											</tr>
+											<tr>
+												<td>Admin UserName</td>
+												<td>: <?php  echo $row['AdminUserName'];?></td>
+											</tr>
+											<tr>
+												<td>First Name</td>
+												<td>: <?php  echo $row['AFirstName_en'];?></td>
+											</tr>
+											<tr>
+												<td>Last Name</td>
+												<td>: <?php  echo $row['ALastName_en'];?></td>
+											</tr>
+											<tr>
+												<td>DOB</td>
+												<td>: <?php  echo $row['DoB'];?></td>
+											</tr>
+											<tr>
+												<td>Full Name</td>
+												<td>: <?php  echo $row['AFullName_en'];?></td>
+											</tr>
+											<tr>
+												<td class="BoldText">Mobile</td>
+												<td>: <?php  echo $row['Mobile'];?></td>
+											</tr>
+
+										</table>
+									</div>
+								</div>
+							</div>							
 						</div>
 					</div>
-				</section><!-- /.content -->
-						<?php }?>
+				</section>
+						
 			</div><!-- /.content-wrapper -->
 
 			<footer class="main-footer">
@@ -117,5 +114,7 @@ if($_SESSION['AdminId']==''){
 		<!-- Swcc App -->
 		<script src="dist/js/app.min.js" type="text/javascript"></script>
 		<!-- Swcc for demo purposes -->
+
+<?php include('Admin_files/includes/footer.php') ?>
 	</body>
 </html>
