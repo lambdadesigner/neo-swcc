8b71e178d3427e643489440824a80478206f648d
lambdainnovphp <mydihemasundar@lambdainnov.com>
2015-09-22 16:31:36 +0530

    admin donut
    donut design refined

--- ddonut.php
+++ ddonut.php
@@ -11,187 +11,99 @@ include('includes/database.php');
 	{
 		$startD = date("Y");
 		$EndD = date("Y");		
-	}
-	$instAttend = "SELECT * FROM AttendanceDelegation where OriginalInstructor='".$_GET['Inuser']."' AND ClassDate >= '".$startD."0101' AND ClassDate <=  '".$EndD."1231'"; 
-	$instAttend_result = sqlsrv_query( $conn, $instAttend ,array(), array( "Scrollable" => SQLSRV_CURSOR_KEYSET ));	
-	$row_countss = sqlsrv_num_rows($instAttend_result);	 		
-	if($row_countss > 0)
-	{
+	}	
 ?>
 
 <script type="text/javascript">
-	// create some data
-	var seedData = [	
-	<?php
-		while($instAttend_data = sqlsrv_fetch_array($instAttend_result)){
-		$Imonth = explode('-',date_format($instAttend_data['ClassDate'],'Y-m-d')); 		
+// create some data
+var seedData = [
 
-	  if($Imonth[1]=="01"){
-	  	$instAttend1 = "SELECT * FROM AttendanceDelegation where OriginalInstructor='".$_GET['Inuser']."' AND ClassDate >= '".$startD."0101' AND ClassDate <=  '".$EndD."0131'"; 
-	 	$instAttend_result1 = sqlsrv_query( $conn, $instAttend1 ,array(), array( "Scrollable" => SQLSRV_CURSOR_KEYSET ));
-	 	$row_count1 = sqlsrv_num_rows($instAttend_result1);	 		
-	 ?>
-	 {
-	    "label": "January",
-	    "value": Math.round(<?php echo $row_count1;?>)
-	 },
-	 <?php } else{?>
-	{
-	   "label": "January",
-	   "value": Math.round(0)
-	},
-	<?php } if($Imonth[1]=="02"){
-	 	$instAttend2 = "SELECT * FROM AttendanceDelegation where OriginalInstructor='".$_GET['Inuser']."' AND ClassDate >= '".$startD."0201' AND ClassDate <=  '".$EndD."0229'"; 
-	 	$instAttend_result2 = sqlsrv_query( $conn, $instAttend2 ,array(), array( "Scrollable" => SQLSRV_CURSOR_KEYSET ));
-	 	$row_count2 = sqlsrv_num_rows($instAttend_result2);	?> 
-	 {
-	    "label": "February",
-	    "value": Math.round(<?php echo $row_count2;?>)
-	 },
-	<?php } else{?> 
-	{
-	   "label": "February",
-	   "value": Math.round(0)
-	},
-	<?php } if($Imonth[1]=="03"){
-		$instAttend3 = "SELECT * FROM AttendanceDelegation where OriginalInstructor='".$_GET['Inuser']."' AND ClassDate >= '".$startD."0301' AND ClassDate <=  '".$EndD."0331'"; 
-		$instAttend_result3 = sqlsrv_query( $conn, $instAttend3 ,array(), array( "Scrollable" => SQLSRV_CURSOR_KEYSET ));
-		$row_count3 = sqlsrv_num_rows($instAttend_result3);?> 
-	{
-	   "label": "March",
-	   "value": Math.round(<?php echo $row_count3;?>)
-	},
-	<?php } else{?> 
-	{
-	   "label": "March",
-	   "value": Math.round(0)
-	},
-	 <?php } if($Imonth[1]=="04"){
-	 	$instAttend4 = "SELECT * FROM AttendanceDelegation where OriginalInstructor='".$_GET['Inuser']."' AND ClassDate >= '".$startD."0401' AND ClassDate <=  '".$EndD."0430'"; 
-	 	$instAttend_result4 = sqlsrv_query( $conn, $instAttend4 ,array(), array( "Scrollable" => SQLSRV_CURSOR_KEYSET ));
-	 	$row_count4 = sqlsrv_num_rows($instAttend_result4);?>  
-	 {
-	    "label": "April",
-	    "value": Math.round(<?php echo $row_count4;?>)
-	 }, 
-	<?php } else{?> 
-	{
-	   "label": "April",
-	   "value": Math.round(0)
-	},
-	 <?php } if($Imonth[1]=="05"){
-	 	$instAttend5 = "SELECT * FROM AttendanceDelegation where OriginalInstructor='".$_GET['Inuser']."' AND ClassDate >= '".$startD."0501' AND ClassDate <=  '".$EndD."0531'"; 
-	 	$instAttend_result5 = sqlsrv_query( $conn, $instAttend5 ,array(), array( "Scrollable" => SQLSRV_CURSOR_KEYSET ));
-	 	$row_count5 = sqlsrv_num_rows($instAttend_result5);?> 
-	 {
-	    "label": "May",
-	    "value": Math.round(<?php echo $row_count5;?>)
-	 }, 
-	 <?php } else{?> 
-	{
-	   "label": "May",
-	   "value": Math.round(0)
-	},
-	 <?php } if($Imonth[1]=="06"){
-	 	$instAttend6 = "SELECT * FROM AttendanceDelegation where OriginalInstructor='".$_GET['Inuser']."' AND ClassDate >= '".$startD."0601' AND ClassDate <=  '".$EndD."0630'"; 
-	 	$instAttend_result6 = sqlsrv_query( $conn, $instAttend6 ,array(), array( "Scrollable" => SQLSRV_CURSOR_KEYSET ));
-	 	$row_count6 = sqlsrv_num_rows($instAttend_result6);?> 
-	 {
-	    "label": "June",
-	    "value": Math.round(<?php echo $row_count6;?>)
-	 }, 
-	<?php } else{?> 
-	{
-	   "label": "June",
-	   "value": Math.round(0)
-	}, 
-	 <?php } if($Imonth[1]=="07"){
-	 	$instAttend7 = "SELECT * FROM AttendanceDelegation where OriginalInstructor='".$_GET['Inuser']."' AND ClassDate >= '".$startD."0701' AND ClassDate <=  '".$EndD."0731'"; 
-	 	$instAttend_result7 = sqlsrv_query( $conn, $instAttend7 ,array(), array( "Scrollable" => SQLSRV_CURSOR_KEYSET ));
-	 	$row_count7 = sqlsrv_num_rows($instAttend_result7);?> 
-	 {
-	    "label": "July",
-	    "value": Math.round(<?php echo $row_count7;?>)
-	 }, 
-	 <?php } else{?> 
-	{
-	   "label": "July",
-	   "value": Math.round(0)
-	}, 
-	 <?php } if($Imonth[1]=="08"){
-	 	$instAttend8 = "SELECT * FROM AttendanceDelegation where OriginalInstructor='".$_GET['Inuser']."' AND ClassDate >= '".$startD."0801' AND ClassDate <=  '".$EndD."0831'"; 
-	 	$instAttend_result8 = sqlsrv_query( $conn, $instAttend8 ,array(), array( "Scrollable" => SQLSRV_CURSOR_KEYSET ));
-	 	$row_count8 = sqlsrv_num_rows($instAttend_result8);?> 
-	 {
-	    "label": "August",
-	    "value": Math.round(<?php echo $row_count8;?>)
-	 }, 
-	 <?php } else{?> 
-	{
-	   "label": "August",
-	   "value": Math.round(0)
-	}, 
-	 <?php } if($Imonth[1]=="09"){
-	 	$instAttend9 = "SELECT * FROM AttendanceDelegation where OriginalInstructor='".$_GET['Inuser']."' AND ClassDate >= '".$startD."0901' AND ClassDate <=  '".$EndD."0930'"; 
-	 	$instAttend_result9 = sqlsrv_query( $conn, $instAttend9 ,array(), array( "Scrollable" => SQLSRV_CURSOR_KEYSET ));
-	 	$row_count9 = sqlsrv_num_rows($instAttend_result9);?> 
-	 {
-	    "label": "September",
-	    "value": Math.round(<?php echo $row_count9;?>)
-	 }, 
-	 <?php } else{?> 
-	{
-	   "label": "September",
-	   "value": Math.round(0)
-	}, 
-	 <?php } if($Imonth[1]=="10"){
-	 	$instAttend10 = "SELECT * FROM AttendanceDelegation where OriginalInstructor='".$_GET['Inuser']."' AND ClassDate >= '".$startD."1001' AND ClassDate <=  '".$EndD."1031'"; 
-	 	$instAttend_result10 = sqlsrv_query( $conn, $instAttend10 ,array(), array( "Scrollable" => SQLSRV_CURSOR_KEYSET ));
-	 	$row_count10 = sqlsrv_num_rows($instAttend_result10);?> 
-	 {
-	    "label": "October",
-	    "value": Math.round(<?php echo $row_count10;?>)
-	 }, 
-	 <?php } else{?> 
-	{
-	   "label": "October",
-	   "value": Math.round(0)
-	}, 
-	 <?php } if($Imonth[1]=="11"){
-	 	$instAttend11 = "SELECT * FROM AttendanceDelegation where OriginalInstructor='".$_GET['Inuser']."' AND ClassDate >= '".$startD."1101' AND ClassDate <=  '".$EndD."1130'"; 
-	 	$instAttend_result11 = sqlsrv_query( $conn, $instAttend11 ,array(), array( "Scrollable" => SQLSRV_CURSOR_KEYSET ));
-	 	$row_count11 = sqlsrv_num_rows($instAttend_result11);?> 
-	 {
-	    "label": "November",
-	    "value": Math.round(<?php echo $row_count11;?>)
-	 }, 
-	 <?php } else{?> 
-	{
-	   "label": "November",
-	   "value": Math.round(0)
-	}, 
-	 <?php } if($Imonth[1]=="12"){
-	 	$instAttend12 = "SELECT * FROM AttendanceDelegation where OriginalInstructor='".$_GET['Inuser']."' AND ClassDate >= '".$startD."1201' AND ClassDate <=  '".$EndD."1231'"; 
-	 	$instAttend_result12 = sqlsrv_query( $conn, $instAttend12 ,array(), array( "Scrollable" => SQLSRV_CURSOR_KEYSET ));
-	 	$row_count12 = sqlsrv_num_rows($instAttend_result12);?> 
-	 {
-	    "label": "December",
-	    "value": Math.round(<?php echo $row_count12;?>)
-	 },
-	 <?php } else{?> 
-	{
-	   "label": "December",
-	   "value": Math.round(0)
-	},
-	<?php } 
+<?php
+	$instAttend1 = "SELECT * FROM AttendanceDelegation where OriginalInstructor='".$_GET['Inuser']."' AND ClassDate >= '".$startD."0101' AND ClassDate <=  '".$EndD."0131'"; 
+ 	$instAttend_result1 = sqlsrv_query( $conn, $instAttend1 ,array(), array( "Scrollable" => SQLSRV_CURSOR_KEYSET ));
+ 	$row_count1 = sqlsrv_num_rows($instAttend_result1);	 	
 
-	goto a;
-	
-	}
-	a:?>
+ 	$instAttend2 = "SELECT * FROM AttendanceDelegation where OriginalInstructor='".$_GET['Inuser']."' AND ClassDate >= '".$startD."0201' AND ClassDate <=  '".$EndD."0228'"; 
+ 	$instAttend_result2 = sqlsrv_query( $conn, $instAttend2 ,array(), array( "Scrollable" => SQLSRV_CURSOR_KEYSET ));
+ 	$row_count2 = sqlsrv_num_rows($instAttend_result2);
 
+ 	$instAttend3 = "SELECT * FROM AttendanceDelegation where OriginalInstructor='".$_GET['Inuser']."' AND ClassDate >= '".$startD."0301' AND ClassDate <=  '".$EndD."0331'"; 
+	$instAttend_result3 = sqlsrv_query( $conn, $instAttend3 ,array(), array( "Scrollable" => SQLSRV_CURSOR_KEYSET ));
+	$row_count3 = sqlsrv_num_rows($instAttend_result3);	
 
-	];
+	$instAttend4 = "SELECT * FROM AttendanceDelegation where OriginalInstructor='".$_GET['Inuser']."' AND ClassDate >= '".$startD."0401' AND ClassDate <=  '".$EndD."0430'"; 
+ 	$instAttend_result4 = sqlsrv_query( $conn, $instAttend4 ,array(), array( "Scrollable" => SQLSRV_CURSOR_KEYSET ));
+ 	$row_count4 = sqlsrv_num_rows($instAttend_result4);
+
+ 	$instAttend5 = "SELECT * FROM AttendanceDelegation where OriginalInstructor='".$_GET['Inuser']."' AND ClassDate >= '".$startD."0501' AND ClassDate <=  '".$EndD."0531'"; 
+ 	$instAttend_result5 = sqlsrv_query( $conn, $instAttend5 ,array(), array( "Scrollable" => SQLSRV_CURSOR_KEYSET ));
+ 	$row_count5 = sqlsrv_num_rows($instAttend_result5);
+
+ 	$instAttend6 = "SELECT * FROM AttendanceDelegation where OriginalInstructor='".$_GET['Inuser']."' AND ClassDate >= '".$startD."0601' AND ClassDate <=  '".$EndD."0630'"; 
+ 	$instAttend_result6 = sqlsrv_query( $conn, $instAttend6 ,array(), array( "Scrollable" => SQLSRV_CURSOR_KEYSET ));
+ 	$row_count6 = sqlsrv_num_rows($instAttend_result6);
+
+ 	$instAttend7 = "SELECT * FROM AttendanceDelegation where OriginalInstructor='".$_GET['Inuser']."' AND ClassDate >= '".$startD."0701' AND ClassDate <=  '".$EndD."0731'"; 
+ 	$instAttend_result7 = sqlsrv_query( $conn, $instAttend7 ,array(), array( "Scrollable" => SQLSRV_CURSOR_KEYSET ));
+ 	$row_count7 = sqlsrv_num_rows($instAttend_result7);
+
+ 	$instAttend8 = "SELECT * FROM AttendanceDelegation where OriginalInstructor='".$_GET['Inuser']."' AND ClassDate >= '".$startD."0801' AND ClassDate <=  '".$EndD."0831'"; 
+ 	$instAttend_result8 = sqlsrv_query( $conn, $instAttend8 ,array(), array( "Scrollable" => SQLSRV_CURSOR_KEYSET ));
+ 	$row_count8 = sqlsrv_num_rows($instAttend_result8);
+
+ 	$instAttend9 = "SELECT * FROM AttendanceDelegation where OriginalInstructor='".$_GET['Inuser']."' AND ClassDate >= '".$startD."0901' AND ClassDate <=  '".$EndD."0930'"; 
+ 	$instAttend_result9 = sqlsrv_query( $conn, $instAttend9 ,array(), array( "Scrollable" => SQLSRV_CURSOR_KEYSET ));
+ 	$row_count9 = sqlsrv_num_rows($instAttend_result9);
+
+ 	$instAttend10 = "SELECT * FROM AttendanceDelegation where OriginalInstructor='".$_GET['Inuser']."' AND ClassDate >= '".$startD."1001' AND ClassDate <=  '".$EndD."1031'"; 
+ 	$instAttend_result10 = sqlsrv_query( $conn, $instAttend10 ,array(), array( "Scrollable" => SQLSRV_CURSOR_KEYSET ));
+ 	$row_count10 = sqlsrv_num_rows($instAttend_result10);
+
+ 	$instAttend11 = "SELECT * FROM AttendanceDelegation where OriginalInstructor='".$_GET['Inuser']."' AND ClassDate >= '".$startD."1101' AND ClassDate <=  '".$EndD."1130'"; 
+ 	$instAttend_result11 = sqlsrv_query( $conn, $instAttend11 ,array(), array( "Scrollable" => SQLSRV_CURSOR_KEYSET ));
+ 	$row_count11 = sqlsrv_num_rows($instAttend_result11);
+
+ 	$instAttend12 = "SELECT * FROM AttendanceDelegation where OriginalInstructor='".$_GET['Inuser']."' AND ClassDate >= '".$startD."1201' AND ClassDate <=  '".$EndD."1231'"; 
+ 	$instAttend_result12 = sqlsrv_query( $conn, $instAttend12 ,array(), array( "Scrollable" => SQLSRV_CURSOR_KEYSET ));
+ 	$row_count12 = sqlsrv_num_rows($instAttend_result12);
+?>
+{
+   "label": "January",
+   "value": Math.round(<?php echo $row_count1;?>)
+}, {
+   "label": "February",
+   "value": Math.round(<?php echo $row_count2;?>)
+}, {
+   "label": "March",
+   "value": Math.round(<?php echo $row_count3;?>)
+}, {
+   "label": "April",
+   "value": Math.round(<?php echo $row_count4;?>)
+}, {
+   "label": "May",
+   "value": Math.round(<?php echo $row_count5;?>)
+}, {
+   "label": "June",
+   "value": Math.round(<?php echo $row_count6;?>)
+}, {
+   "label": "July",
+   "value": Math.round(<?php echo $row_count7;?>)
+}, {
+   "label": "August",
+   "value": Math.round(<?php echo $row_count8;?>)
+}, {
+   "label": "September",
+   "value": Math.round(<?php echo $row_count9;?>)
+}, {
+   "label": "October",
+   "value": Math.round(<?php echo $row_count10;?>)
+}, {
+   "label": "November",
+   "value": Math.round(<?php echo $row_count11;?>)
+}, {
+   "label": "December",
+   "value": Math.round(<?php echo $row_count12;?>)
+}];
 
 	// create tooltip
 	var tooltip = d3.select('#chart')
@@ -303,6 +215,3 @@ include('includes/database.php');
 	  .attr('y', legendRectSize - legendSpacing)
 	  .text(function(d,i) { return seedData[i].label + ' (' + seedData[i].value +')'; });
 	</script>
-<?php } else { 
-	echo "Empty";
-} ?>
